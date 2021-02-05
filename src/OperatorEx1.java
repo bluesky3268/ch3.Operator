7d47bdc21a844cc6e015df5eeb8778b75e2a6486
HYUNBIN_CHO <bluesky3268@gmail.com>
2021-02-05 23:07:49 +0900

    first commit

---
+++ src/OperatorEx1.java
@@ -0,0 +1,11 @@
+public class OperatorEx1 {
+    public static void main(String[] args) throws Exception {
+        int i = 5;
+        i++;
+        System.out.println(i);
+
+        i = 5;
+        ++i;
+        System.out.println(i);
+    }
+}
